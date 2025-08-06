861089bb4065d2fc6d61fd1b656243705bc61405
000x91 <octareenroon91@gmail.com>
2025-08-05 19:52:46 -0500

    Create v8.js

---
+++ scripts/v8.js
@@ -0,0 +1,7 @@
+//v8 brought changes in tech visibility across planets
+
+Events.on(WorldLoadEvent‎, event => { 
+  //do_the_thing();
+  // make erekir and serpulo tech visible on asteroidgen planets
+  // make asteroidgen tech visible mutually between planets
+  })
